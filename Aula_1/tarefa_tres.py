852bc3804d68f5acb09a991e37eeb7f2ca5f1049
AydomSantos <aydomaparecido@gmail.com>
2025-04-01 21:09:53 -0300

    adcionando a tarefa tres

---
+++ Aula_1/tarefa_tres.py
@@ -0,0 +1,6 @@
+
+
+vValor_um = int(input("Digite o primeiro valor: "))
+vValor_dois = int(input("Digite o segundo valor: "))
+vSoma = vValor_um + vValor_dois
+print(f" A soma de {vValor_um} e {vValor_dois} é igual a {vSoma}")
