d358cd8a01afc44bf634198ea94265ad73999e71
AydomSantos <aydomaparecido@gmail.com>
2025-04-03 20:26:03 -0300

    ok

---
+++ Aula_1/tarefa_dez.py
@@ -0,0 +1,3 @@
+vGraus_celsius = float(input("Digite a temperatura em graus Celsius: "))
+vGraus_fahrenheit = (vGraus_celsius * 9/5) + 32
+print("A temperatura em graus Fahrenheit é:", vGraus_fahrenheit)
