02eee27ad58911dfedb205d0e5369e51abd9c1ca
AydomSantos <aydomaparecido@gmail.com>
2025-04-09 19:18:43 -0300

    atualizando os exercicios

---
+++ estrutura_de_decisao/exe_decPrimeiro.py
@@ -0,0 +1,10 @@
+
+vHorario = input("Digite o seu Orario M | V | N: ").lower()
+if vHorario == 'm':
+    print("Bom dia")
+elif vHorario == 'v':
+    print("Boa Tarde")
+elif vHorario == 'n':
+    print("Boa Noite")
+else:
+    print("Valor Invalido")
