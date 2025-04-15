28caa577f46b7743b8bdef3341c2e1c336a1d53e
AydomSantos <aydomaparecido@gmail.com>
2025-04-14 21:27:02 -0300

    exercicio valida valor

---
+++ estrutura_de_funcoes/exe_quatro.py
@@ -0,0 +1,9 @@
+vValor = int(input("Digite um valor: "))
+
+def validaNum(pValor):
+    if pValor <= 0 :
+        print("N")
+    else:
+        print("P")
+
+validaNum(vValor)
---
+++ estrutura_de_funcoes/exe_tres.py
@@ -0,0 +1,8 @@
+vValor_um = int(input("Digite um valor: "))
+vValor_dois = int(input("Digite o segundo valor: "))
+vValor_tres = int(input("Digite o terceiro valor: "))
+
+def soma(pValor_um, pValor_dois, pValor_tres):
+    return print(f"o valor somado e {pValor_um + pValor_dois + pValor_tres}")
+
+soma(vValor_um, vValor_dois, vValor_tres)
